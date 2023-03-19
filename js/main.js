4ec02709a624d876faa080822fc3284e8caf6f58
Brian Salchert <brisalchert@gmail.com>
2023-03-19 12:10:57 -0400

    Added event listeners for arrows being clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+const leftArrow = document.getElementById(`left-arrow`);
+const rightArrow = document.getElementById(`right-arrow`);
+const carouselSlides = document.getElementsByClassName(`carousel-slides`)[0];
+
+let slidesLeft = carouselSlides.clientLeft;
+
 window.onload = () => {
     alert(`Test`);
 };
+
+leftArrow.addEventListener(`click`, () => {
+    carouselSlides.style.transform = `translateX(${slidesLeft -= 680}px)`;
+
+    if (slidesLeft === -2040) {
+        leftArrow.style.visibility = `hidden`;
+    }
+
+    if (slidesLeft === -680) {
+        rightArrow.style.visibility = `visible`;
+    }
+});
+
+rightArrow.addEventListener(`click`, () => {
+    carouselSlides.style.transform = `translateX(${slidesLeft += 680}px)`;
+
+    if (slidesLeft === 0) {
+        rightArrow.style.visibility = `hidden`;
+    }
+
+    if (slidesLeft === -1360) {
+        leftArrow.style.visibility = `visible`;
+    }
+});
